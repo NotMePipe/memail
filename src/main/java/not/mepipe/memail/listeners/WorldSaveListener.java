ed5854810e72f7f26b6c1ff38e6023053142fdb6
MePipe <89614413+NotMePipe@users.noreply.github.com>
2024-04-01 07:37:08 -0400

    Remove saving broadcast

--- src/main/java/not/mepipe/memail/listeners/WorldSaveListener.java
+++ src/main/java/not/mepipe/memail/listeners/WorldSaveListener.java
@@ -1,10 +1,6 @@
 package not.mepipe.memail.listeners;
 
-import net.kyori.adventure.text.format.TextColor;
 import not.mepipe.memail.Main;
-import not.mepipe.memail.utils.ChatComponent;
-import not.mepipe.memail.utils.IndexedHashmap;
-import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.world.WorldSaveEvent;
@@ -13,12 +9,8 @@ public class WorldSaveListener implements Listener {
 
     @EventHandler
     public void onWorldSave(WorldSaveEvent e) {
-        IndexedHashmap<String, TextColor> message = new IndexedHashmap<>();
-        message.add("Game data saved", TextColor.color(170, 170, 170));
-
         if(e.getWorld().getName().equals("world_the_end")) {
             Main.getPlugin().saveData();
-            Bukkit.broadcast(ChatComponent.build(message));
         }
     }
 }
