712cbd429118c1a6319c8dd045d89c6267afb042
MePipe <89614413+NotMePipe@users.noreply.github.com>
2024-04-27 08:32:51 -0400

    Fix config issues

--- src/main/java/not/mepipe/memail/Main.java
+++ src/main/java/not/mepipe/memail/Main.java
@@ -4,9 +4,12 @@ import not.mepipe.memail.commands.MailCommand;
 import not.mepipe.memail.listeners.PlayerJoinListener;
 import not.mepipe.memail.listeners.WorldSaveListener;
 import not.mepipe.memail.managers.MailManager;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
+
+    FileConfiguration config = getConfig();
 
     private static Main plugin;
 
@@ -15,7 +18,8 @@ public final class Main extends JavaPlugin {
     @Override
     public void onEnable() {
         plugin = this;
-
+        config.options().copyDefaults(true);
+        saveDefaultConfig();
         registerListeners();
         registerManagers();
         registerCommands();
--- src/main/java/not/mepipe/memail/listeners/PlayerJoinListener.java
+++ src/main/java/not/mepipe/memail/listeners/PlayerJoinListener.java
@@ -2,6 +2,7 @@ package not.mepipe.memail.listeners;
 
 import net.kyori.adventure.identity.Identity;
 import net.kyori.adventure.text.format.TextColor;
+import not.mepipe.memail.Main;
 import not.mepipe.memail.managers.MailManager;
 import not.mepipe.memail.utils.ChatComponent;
 import not.mepipe.memail.utils.IndexedHashmap;
@@ -15,13 +16,16 @@ public class PlayerJoinListener implements Listener {
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent e) {
-        IndexedHashmap<String, TextColor> message = new IndexedHashmap<>();
-        if (manager.getPlayerUnreadMail(e.getPlayer()).isEmpty()) {
-            message.add("You have no new mail", TextColor.color(255, 170, 0));
-        } else {
-            message.add("You have " + manager.getPlayerUnreadMail(e.getPlayer()).size() + " unread new mail", TextColor.color(255, 170, 0));
+        if (Main.getPlugin().getConfig().getBoolean("display-unread-mail-on-join")) {
+            IndexedHashmap<String, TextColor> message = new IndexedHashmap<>();
+
+            if (manager.getPlayerUnreadMail(e.getPlayer()).isEmpty()) {
+                message.add("You have no new mail", TextColor.color(255, 170, 0));
+            } else {
+                message.add("You have " + manager.getPlayerUnreadMail(e.getPlayer()).size() + " unread new mail", TextColor.color(255, 170, 0));
+            }
+            e.getPlayer().sendMessage(Identity.nil(), ChatComponent.build(message));
         }
-        e.getPlayer().sendMessage(Identity.nil(), ChatComponent.build(message));
     }
 
 }
